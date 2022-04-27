d2cde03c9d389fc7b469b4cf1c82fa4667f67156
Rachel Simonsen <rachellsimonsen@gmail.com>
2022-04-27 11:08:01 -0600

    taking in user name

--- Game.java
+++ Game.java
@@ -9,8 +9,9 @@ public class Game {
     Scanner myObj = new Scanner(System.in);
     System.out.println("Hello, what's your name?");
 
-    String userName = myObj.nextline();
+    String userName = myObj.nextLine();
     System.out.println(userName + ", I'm thinking of a number between 1 and 100. Try to guess my number.");
+
 
   }
 
