461ab108287d0cbe4ef4a6fd19081db746854d5f
Rachel Simonsen <rachellsimonsen@gmail.com>
2022-04-27 10:53:22 -0600

    Began the game buildout

---
+++ Game.java
@@ -0,0 +1,17 @@
+/*
+
+A number-guessing game.
+
+*/
+import java.util.Scanner;  //import the scanner class
+public class Game {
+  public static void main(String[] args){
+    Scanner myObj = new Scanner(System.in);
+    System.out.println("Hello, what's your name?");
+
+    String userName = myObj.nextline();
+    System.out.println(userName + ", I'm thinking of a number between 1 and 100. Try to guess my number.");
+
+  }
+
+}
